82a0c0e889fa0f9fee041b41f3193f8aec125973
xan <62293102+xan-crypto@users.noreply.github.com>
2022-05-25 21:20:19 +0200

    Update CurveBootstrap.py

--- Services/CurveBootstrap/CurveBootstrap.py
+++ Services/CurveBootstrap/CurveBootstrap.py
@@ -2,18 +2,19 @@
 # @title CurveBootstrap.py
 # @dev This script will bootstrap the crypto native USD yield curve
 # since there is no well defined USD yield curve in crypto, below is how we suggest the bootstrap process be done
-# we will use the following rates to build a 5y crypto native USD yield curve
-# - AAVE will be used for ON rate
-# - Deribit futures will be used for 0-1yr, because of the option market, the longer dated futures here should be more representative of rates
+# we will use the following rates to build a 12m crypto native USD yield curve
+# - AAVE USDC borrow will be used for the ON rate (1d)
+# - deribit futures will be used for 0-12m, because of the option market, the longer dated futures here should be more representative of rates
 # - deribit is also longer term vs binance / ftx which maxs out at about 6 months
-# - Post 1yr we will use a ratioed shift from US treasuries, we will use 0.5/1/2/3/5 US treasury rates
+# - pricing oracle to add robustness to the above rates (e.g. add compound for ON, or FTX/Binance for spot futs)
+# - we use a margin fraction for spot futs basis to create lend / borrow double
+# - implies x% of cash needed to sustain margin position => lend rate/(1-x%)
+# - to be added later, post 1yr we will use a ratioed shift from US treasuries, we will use 0.5/1/2/3/5 US treasury rates
 # - the ratioed shift should be appropriate because the treasuries are risk free so the shape should be correct
 # - the CurveZero curve is based on over collateralisation hence there should also be no market risk
 # - by using the shift from the 1y pt we will capture the native crypto premium but maintain the risk free shape
-# 23-05-22 updates
-# - remove treasury bonds for simplicity
-# - add margin frac for spot futs basis to create lend / borrow double
-# - implies x% of cash needed to sustain position => lend rate/(1-x%)
+# - for now we remove treasury bonds for simplicity
+# - put limits on min and max rate
 # @author xan-crypto
 ####################################################################################
 
@@ -26,7 +27,7 @@ import time
 import investpy
 from matplotlib import pyplot as plt
 
-# @dev aave ON rate on USDC - get from graphql
+# @dev aave ON rate on USDC borrow rate - get from graphql
 def get_aave_on(_yc):
     query = """ query {reserves(where: {id: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb480xb53c1a33016b2dc2ff3653530bff1848a515c8c5"}) {name averageStableRate stableBorrowRate variableBorrowRate}} """
     response = requests.request('post', 'https://api.thegraph.com/subgraphs/name/aave/protocol-v2',json={'query': query})
@@ -34,7 +35,7 @@ def get_aave_on(_yc):
     _yc = _yc.append({'source': 'aave','type': 'on','term': 1,'rate': int(response['data']['reserves'][0]['variableBorrowRate']) / 10 ** 27}, ignore_index=True)
     return _yc
 
-# @dev deribit for the futs
+# @dev deribit for the spot futs basis
 def get_deribit_futs(_yc):
     deribit = ccxt.deribit({'rateLimit': 100, 'options': {'adjustForTimeDifference': True}, 'enableRateLimit': True})
     tickers = deribit.public_get_get_instruments({'currency':'BTC','kind':'future', 'expired': False})['result']
@@ -46,6 +47,7 @@ def get_deribit_futs(_yc):
             mid_price = (float(price['result']['best_bid_price']) + float(price['result']['best_ask_price']))*0.5
             term = (float(ticker['expiration_timestamp'])/1000-now)/86400
             # @dev to prevent distorts vs ON from aave, less than 14 days on deribit can behave strangely
+            # since close to expiry spot and futs differences can results in large positive / negative rates
             # spots futs basis is lend side, to get borrow side assume 20% of realised cash needed for margin
             if term > 14:
                 rate = ((mid_price/index)**(365/term)-1)/0.8
@@ -56,7 +58,7 @@ def get_deribit_futs(_yc):
 # @dev investing for treasury bonds
 def get_investing_tbonds(_yc):
     tb = investpy.bonds.get_bonds_overview('united states', as_json=False)
-    index = tb.loc[tb.name=='U.S. 2Y'].index[0]
+    index = tb.loc[tb.name=='U.S. 5Y'].index[0]
     tb = tb[0:index+1]
     tb['last'] = tb['last']/100
     tb['term'] = 0.0
@@ -72,11 +74,16 @@ def get_investing_tbonds(_yc):
     return _yc
 
 def bootstrap_yc():
-    _yc = pd.DataFrame(columns=['source', 'type', 'term', 'rate'])
+    _yc = pd.DataFrame(columns=['source', 'type', 'term', 'rate', 'yfrac', 'ts_capture', 'ts_expiry'])
     _yc = get_aave_on(_yc)
     _yc = get_deribit_futs(_yc)
     # _yc = get_investing_tbonds(_yc)
     _yc['yfrac'] = _yc.term / 365
+    _yc['ts_capture'] = time.time()
+    _yc['ts_expiry'] = _yc.term * 86400 + time.time()
+    # @dev to prevent arbitrage, the curve will be flat from Aave ON or upward sloping
+    # reject any rates below Aave ON
+    _yc = _yc.loc[_yc.rate >= _yc.iloc[0]['rate']].reset_index(drop=True)
     return _yc
 
 # @dev plot curve
